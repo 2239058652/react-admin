48ece5eacb021515cb8f1794937ee8a41337703c
xiaoding <2239058652@qq.com>
2025-02-25 17:33:09 +0800

    安装sass配置相关文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import './App.css'
-import React from 'react'
+import './App.scss'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      '初始化'
+      <div onClick={() =>setCount(0)}>count: {count}</div>
     </>
   )
 }
