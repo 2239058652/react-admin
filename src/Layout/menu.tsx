3347d50b186b9b786a97dd71c641d851645601c6
xiaoding <2239058652@qq.com>
2025-02-28 11:45:58 +0800

    menu deit

--- src/Layout/menu.tsx
+++ src/Layout/menu.tsx
@@ -1,7 +1,13 @@
 import { Menu } from 'antd';
 import { Outlet, Link } from 'react-router-dom';
+import routes from '@/router';
+import { useEffect } from 'react';
 
 function menu() {
+    useEffect(() => {
+        console.log(routes);
+    }, []);
+
     return (
         <Menu
             theme="light"
