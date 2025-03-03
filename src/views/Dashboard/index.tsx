1b7d63c9f6b4601cbef77e4d7608f348a56a60c4
xiaoding <2239058652@qq.com>
2025-03-03 15:34:35 +0800

    修改dashed

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
+import { Input } from 'antd'
 
 const Dashboard: React.FC = () => {
-    return (
-        <div>
-            <h1>仪表盘</h1>
-            <p>这是仪表盘页面。</p>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>仪表盘</h1>
+      <p>这是仪表盘页面。</p>
+      <Input placeholder="Basic usage" />
+    </div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
